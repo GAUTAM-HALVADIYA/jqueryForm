401b3910e8071f69aa8a1c5eb41e7721373e4e34
GAUTAM-HALVADIYA <halvadiyagautam57@gmail.com>
2026-04-23 18:57:25 +0530

    jquery

---
+++ index.js
@@ -0,0 +1,391 @@
+let storage = window.localStorage;
+let localData = JSON.parse(storage.getItem("data")) || [];
+
+let isEdit;
+let submitBtn = $("#sb-button")
+
+printTable()
+
+$("#photo").on("change", function logPhoto(event) {
+    const file = event.target.files[0]; 
+    
+    let img = $("#img");
+    
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const base64String = e.target.result;
+            console.log("Photo Data (Base64):", base64String);
+   
+            img.addClass(".img")
+            img.attr("src", base64String)
+        };
+
+        reader.readAsDataURL(file);
+
+    }
+})
+
+
+$(".form-control").toArray().forEach(input => {
+    $(input).on("keyup", function() {
+        validateSingleField($(this));
+    });
+});
+
+
+submitBtn.on("click", function(){
+    let data = validation()
+
+    if(!data) return
+
+    if(isEdit)
+    {
+        let id = localData.findIndex(element => element.id == isEdit)
+        localData[id] = {id: isEdit, ...data };
+        
+        isEdit = null;
+    }
+    else
+        localData.push({id: Date.now(), ...data })
+
+    storage.setItem("data", JSON.stringify(localData))
+
+    $("#rs-button").click()
+    hideForm()
+    printTable()
+    submitBtn.textContent = "Submit";
+
+});
+
+
+$("#rs-button").on("click", reset);
+
+let regex = /^[a-zA-Z]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+let regPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%&!])[A-Za-z\d@#$%&!]{8,}$/;
+let regName = /^\S[a-zA-Z, .'-]{2,}$/;
+
+function validateSingleField(element) {
+
+  
+    if(element.attr("id") == "full")
+        validateField(element, regName, "At least 3 letters; no starting space.");
+    else if(element.attr("id") == "email")
+        validateField(element, regex, "Enter valid email");
+    else if(element.attr("id") == "password")
+        validateField(element, regPass, "Password must be strong");
+    else if(element.attr("id") == "address")
+        validateField(element, /^.[a-zA-Z0-9 ,.-]{10,}$/,"Please write valid Address at least min 10 character");
+    
+}
+
+
+function validation() {
+
+    let data = getFormData()
+
+    let name = validateField($("#full"), regName, "At least 3 letters; no starting space.");
+    let mail = validateField($("#email"), regex, "Enter valid email");
+    let pass = validateField($("#password"), regPass, "Password must be strong");
+    let address = validateField($("#address"), /^\S[a-zA-Z0-9 ,.-]{9,}$/,"Please write valid Address");
+    
+    
+
+    if(validateOther(data) && name && mail && pass && address){
+        console.log("all fine");
+        console.log(getFormData());
+        return data
+    }
+    else
+        return null
+}
+
+function validateField(element, regex, errorMsg) {
+
+
+
+    if (element.val() === "") {
+        showInValid(element, "Field is mandatory");
+    }
+    else if (!regex.test(element.val())) {
+        showInValid(element, errorMsg);
+    }
+    else {
+        showValid(element, "Looks good!");
+        return true;
+    }
+
+    return false;
+}
+
+function removeFeedback(element) {
+    element.next(".valid-feedback, .invalid-feedback").remove();
+    element.removeClass("is-valid is-invalid");
+}
+
+function showInValid(element, msg) {
+    removeFeedback(element);
+    element.addClass("is-invalid");
+
+    let newMsg = $("<div></div>");
+    newMsg.addClass("invalid-feedback");
+    newMsg.text(msg);
+
+    element.after(newMsg);
+}
+
+function showValid(element, msg) {
+    removeFeedback(element);
+    element.addClass("is-valid");
+
+    let newMsg = $("<div></div>");
+    newMsg.addClass("valid-feedback");
+    newMsg.text(msg);
+
+    element.after(newMsg);
+}
+
+
+$("#date").on("input", age)
+
+function age(e) {
+    const ageElem = $("#age");
+    const dobValue = e.target.value;
+
+    if (!dobValue) return;
+
+    const birthDate = new Date(dobValue);
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    ageElem.val(age);
+
+
+    if (age < 18) 
+       showInValid(ageElem,  "Must be 18 or above")
+    else
+        removeFeedback(ageElem)
+   
+}
+
+function validateOther(data){
+
+    let isValid = true;
+
+    if(!data.bod)
+       isValid = showInValid($("#date"), "DOB is mendatory")
+    else
+        removeFeedback($("#date"))
+
+    if (data.age < 18) 
+       isValid = showInValid($("#age"),  "Must be 18 or above")
+    else
+        removeFeedback($("#age"))
+
+    if(!data.gender)
+       isValid = showInValid($("#last-radio"), "Please select gender")
+    else
+        removeFeedback($("#last-radio"))
+
+    if(!data.hobbies.length)
+       isValid = showInValid($("#last-checkBox"), "Please select your hobbies");
+    else
+        removeFeedback($("#last-checkBox"))
+
+    if(!data.country)
+       isValid = showInValid($("#country"), "Please select country");
+    else
+        removeFeedback($("#country"))
+
+    let terms = $('input[name="terms"]').prop("checked")
+    if(!terms)
+        isValid = showInValid($("#terms"), "Please select terms");
+    else
+        removeFeedback($("#terms"))
+
+    return isValid
+
+}
+
+function reset()
+{
+    $(".invalid-feedback").remove()
+    $(".valid-feedback").remove()
+    $(".is-invalid").removeClass("is-invalid")
+    $(".is-valid").removeClass("is-valid")
+
+    let img = $("#img")
+    img.removeClass(".img")
+    img.attr("src", "")
+    
+}
+
+
+function printTable(given = localData){
+
+    let tblData = $("#tblData")
+    tblData.html(given.map(createRow).join(""));
+
+}
+
+function createRow(element)
+{
+
+    let tableRow = `<tr data-id="${element.id}">
+                        <td>
+                            <div class="wrapper" style="display: flex;">
+                            <img class="card-img" src="${element.image}" alt="Profile" style="width: 50px; height: auto;">
+                            </div>
+                        </td>
+                        <td>${element.id}</td>
+                        <td>${element.name}</td>
+                        <td>${element.email}</td>
+                        <td>${element.bod}</td>
+                        <td>${element.age}</td>
+                        <td>${element.gender}</td>
+                        <td>${element.hobbies.join(", ")}</td>
+                        <td>${element.country}</td>
+                        <td>${element.address}</td>
+                        <td>
+                            <div class="d-flex gap-2">
+                                <button class="btn btn-primary" data-action="edit">Edit</button>
+                                <button class="btn btn-danger" data-action="delete">Delete</button>
+                            </div>
+                        </td>
+                    </tr>`
+    
+    return tableRow
+}
+
+let form = $("#form");
+let box = $("<div></div>");
+
+$("#shw-btn").on("click", showForm)
+
+box.on("click", hideForm)
+
+function hideForm(){
+    $("#rs-button").click()
+    form.css("display", "none");
+    box.detach()
+
+    isEdit = null;
+
+    submitBtn.text("Submit"); 
+
+}
+function showForm(){
+   
+    box.addClass("box-shadow");
+
+    if (form.is(":hidden")) {
+        form.css("display", "flex");
+        form.after(box);
+    } 
+  
+}
+
+
+$("#search").on("input", searchList)
+
+function searchList(){
+    
+    let type = $("#search").val().toLowerCase();
+
+    let searched = localData.filter(obj =>
+        [obj.id, obj.name, obj.email, obj.bod, obj.age, obj.gender, obj.country, obj.address]
+            .some(val => String(val).toLowerCase().includes(type))
+    );
+
+    printTable(searched)
+}
+
+$("#tblData").on("click", "[data-action]", function() {
+
+    let btn = $(this);               
+    let row = btn.closest("tr");    
+
+    const id = row.data("id");
+    const action = btn.data("action");
+
+    if (action === "edit") showData(id);
+    if (action === "delete") deleteRow(id);
+});
+
+
+function showData(id){
+
+    isEdit = id
+    let givenData = localData.find(element => element.id == id)
+
+    $("#full").val(givenData.name)
+    $("#email").val(givenData.email)
+    $("#password").val(givenData.password)   
+    $("#date").val(givenData.bod)
+    $("#age").val(givenData.age)
+    $("#img").attr("src", givenData.image)
+    $("#country").val(givenData.country)
+    $("#address").val(givenData.address)
+ 
+    let gender = $('input[name="RadioOptions"]');
+    gender.each((index, radio) => {
+        $(radio).prop("checked", $(radio).val() == givenData.gender);
+    });
+
+    let hobbies = $('input[name="hobbies"]');
+    hobbies.each((index, check) => {
+        $(check).prop(
+            "checked",
+            givenData.hobbies.includes($(check).val())
+        );
+    });
+
+    showForm()
+    submitBtn.text("Update"); 
+
+}
+
+function deleteRow(id){
+    if(confirm("Are you sure?")){
+        const index = localData.findIndex(el => String(el.id) === String(id));
+        if (index === -1) return;
+
+        localData.splice(index, 1);
+        storage.setItem("data", JSON.stringify(localData));
+        printTable();
+    }
+}
+
+$("#cancel").on("click", "[data-conform]", function cancle(){
+
+    let btn = $(this).data("conform")
+
+    if(btn == "yes")
+    
+    $("#confirm-shadow").hide()
+})
+
+
+function getFormData(){
+   return { 
+        name: $("#full").val(),
+        address: $("#address").val(),
+        age: $("#age").val(),
+        bod: $("#date").val(),
+        country:$("#country").val(),
+        email: $("#email").val(),
+        gender: $('input[name="RadioOptions"]:checked').val(),
+        hobbies: $('input[name="hobbies"]:checked').toArray().map(cb => $(cb).val()),
+        image: $("#img").attr("src") || "./passport.jpg",
+        password: $("#password").val()
+    }
+}
+
+
